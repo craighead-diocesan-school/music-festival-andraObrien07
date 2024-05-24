0dead9b65d28da41018434372b91de17742188b7
andraObrien07 <comanan09@craighead.school.nz>
2024-05-24 15:20:07 +1200

    pages complete
    functional nav that connects the website together

---
+++ src/routes/generalinfo/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Header from "$lib/Header.svelte";
+    import Footer from "$lib/Footer.svelte";
+    import Nav from "$lib/Nav.svelte";
+</script>
+
+<Header />
+<Nav />
+
+<main>
+    <h2>General Information</h2>
+    <img src="/General Information/colin-lloyd-2.jpg" alt="" />
+    <img src="/General Information/jade-masri.jpg" alt="" />
+    <img src="/General Information/samuel-regan-asante.jpg" alt="" />
+    <img src="/General Information/tijs-van-leur.jpg" alt="" />
+</main>
+<Footer />
