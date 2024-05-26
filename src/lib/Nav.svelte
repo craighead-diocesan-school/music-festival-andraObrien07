74daf9929af1a020ee07a332be968a1aa51db5d9
andraObrien07 <comanan09@craighead.school.nz>
2024-05-27 10:41:36 +1200

    nav update
    nav images aligned in the center of yellow nave stripp
    
    next I will space them evenly and add padding or margion on the edges

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,14 +1,17 @@
 <nav>
-    <a href="/"> <img src="Hands - Sun.png" alt="Sun Icon" /></a>
+    <div class="navv">
+        <a href="/"> <img src="Hands - Sun.png" alt="Sun Icon" /></a>
 
-    <a href="/generalinfo">
-        <img src="Brazuca - Screen 4.png" alt="Information Screen" /></a
-    >
-    <a href="/acts"><img src="/The Band - Microphone.png" alt="Microphone" /></a
-    >
-    <a href="/accommodation"
-        ><img src="Beep Beep - Luggage.png" alt="Luggage" /></a
-    >
+        <a href="/generalinfo">
+            <img src="Brazuca - Screen 4.png" alt="Information Screen" /></a
+        >
+        <a href="/acts"
+            ><img src="/The Band - Microphone.png" alt="Microphone" /></a
+        >
+        <a href="/accommodation"
+            ><img src="Beep Beep - Luggage.png" alt="Luggage" /></a
+        >
+    </div>
 </nav>
 
 <style>
@@ -17,6 +20,8 @@
     }
     img {
         width: 130px;
+    }
+    .navv {
         display: flex;
         justify-content: space-between;
         /* justify-items: center; */
