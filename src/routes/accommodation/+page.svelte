1fde5e316e5e408ca94dfa288a5ef1674f54e959
andraObrien07 <comanan09@craighead.school.nz>
2024-05-27 10:51:40 +1200

    accommodation page
    added the text onto the page in prepartion to style the text and to see if the font family works on every page which it does ✅

--- src/routes/accommodation/+page.svelte
+++ src/routes/accommodation/+page.svelte
@@ -13,17 +13,48 @@
         src="/Accommodation/danny-howe-2.jpg"
         alt="behind crowd with white confetti falling"
     />
+
+    <p>
+        Welcome to the ultimate music festival experience! Get ready for a day
+        filled with live music, great food, and unforgettable memories.
+    </p>
     <img
         src="/Accommodation/hanny-naibaho.jpg"
         alt="behind crowd view of stage and pruple lights"
     />
+    <p>
+        Our festival features a diverse lineup of talented musicians from
+        different genres, sure to satisfy every taste in music. With multiple
+        stages set up throughout the venue, you can easily move from one act to
+        the next, discovering new favorites and enjoying classic hits.
+    </p>
     <img
         src="/Accommodation/krists-luhaers.jpg"
         alt="person dancing in front of tents"
     />
+    <p>
+        From up-and-coming artists to established headliners, we have curated an
+        exciting lineup that will keep you dancing and singing along all day
+        long. Our festival is the perfect opportunity to discover new music, see
+        your favorite artists live, and make lasting memories with friends and
+        family.
+    </p>
     <img
         src="/Accommodation/vonecia-carswell.jpg"
         alt="people dancing and talking with eachother"
     />
+    <p>
+        But the music is just the beginning. Our festival also boasts an
+        impressive selection of food vendors, offering everything from classic
+        festival fare to more gourmet options. You won't go hungry with the
+        delicious treats and refreshing beverages available throughout the
+        venue.
+    </p>
 </main>
 <Footer />
+
+<style>
+    p {
+        font-family: "Ibarra Real Nova";
+    }
+</style>
