4aaab7d168f78281928e35f88ce4fd9ea3616c60
andraObrien07 <comanan09@craighead.school.nz>
2024-05-26 16:33:53 +1200

    added photos
    added the photos that teh stakeholder provided

---
+++ src/routes/accommodation/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Header from "$lib/Header.svelte";
+    import Footer from "$lib/Footer.svelte";
+    import Nav from "$lib/Nav.svelte";
+</script>
+
+<Header />
+<Nav />
+
+<main>
+    <h2>General Information</h2>
+    <img src="/Accommodation/danny-howe-2.jpg" alt="" />
+    <img src="/Accommodation/hanny-naibaho.jpg" alt="" />
+    <img src="/Accommodation/krists-luhaers.jpg" alt="" />
+    <img src="/Accommodation/vonecia-carswell.jpg" alt="" />
+</main>
+<Footer />
--- src/routes/acts/+page.svelte
+++ src/routes/acts/+page.svelte
@@ -1,1 +1,17 @@
-lol
+<script>
+    import Header from "$lib/Header.svelte";
+    import Footer from "$lib/Footer.svelte";
+    import Nav from "$lib/Nav.svelte";
+</script>
+
+<Header />
+<Nav />
+
+<main>
+    <h2>General Information</h2>
+    <img src="/Acts/aditya-chinchure.jpg" alt="" />
+    <img src="/Acts/colin-lloyd.jpg" alt="" />
+    <img src="/Acts/danny-howe.jpg" alt="" />
+    <img src="/Acts/maxwell-collins.jpg" alt="" />
+</main>
+<Footer />
